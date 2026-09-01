05f198eee7f856b3ebc75df1f93c50546ee642f4
yashwantsonkar298-sys <yashwantsonkar298-sys@users.noreply.github.com>
2026-09-01 12:07:21 +0530

    Add projects 5 and 6

--- project five/src/footer.jsx
+++
@@ -1,8 +0,0 @@
-export function Footer(){
-    return (
-        <div>
-            <h2>Footer Component </h2>
-            <p>This is exported using  export</p>
-        </div>
-    );
-}
--- project five/src/header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(){
-    return(
-        <div>
-            <h2> Header component</h2>
-            <p>this is exported using default</p>
-        </div>
-    );
-}
-export default Header;
